7b46c31ca4977c983f852624396fd0c8aaaa44e8
waltersmatthew <waltersmatthew88@gmail.com>
2023-08-08 11:23:54 -0700

    finally got localstorage to update on clear

--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -11,6 +11,39 @@ const CartProvider = ({ children }) => {
     useEffect(() => {
         localStorage.setItem("cartItems", JSON.stringify(cartItems));
     }, [cartItems]);
+
+    useEffect(() => {
+        // Watch for changes in local storage made in other tabs/windows
+        const handleStorageChange = (event) => {
+            if (event.key === "cartItems") {
+                const newCartItems = JSON.parse(event.newValue);
+                setCartItems(newCartItems || []);
+            }
+        };
+
+        window.addEventListener("storage", handleStorageChange);
+
+        return () => {
+            window.removeEventListener("storage", handleStorageChange);
+        };
+    }, []);
+
+    // Periodically check for changes in localStorage
+    useEffect(() => {
+        const checkLocalStorageChanges = () => {
+            const newCartItems =
+                JSON.parse(localStorage.getItem("cartItems")) || [];
+            if (JSON.stringify(newCartItems) !== JSON.stringify(cartItems)) {
+                setCartItems(newCartItems);
+            }
+        };
+
+        const interval = setInterval(checkLocalStorageChanges, 1000); // Check every second
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [cartItems]);
     return (
         <CartContext.Provider value={{ cartItems, setCartItems }}>
             {children}
